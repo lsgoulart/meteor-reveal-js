9d2d4cd490203d058a65154ccdd6aea4ba3b3185
Leonardo Goulart <leonardo.sgoulart@gmail.com>
2015-11-11 11:03:31 -0200

    apdate

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'lsgoulart:reveal-js',
   summary: 'Reveal.js - HTML Presentation Framework',
-  version: '3.1.1',
+  version: '3.1.2',
   git: 'https://github.com/lsgoulart/meteor-reveal-js.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.1');
+  api.versionsFrom('1.2');
   api.addFiles([
 	'reveal.js/css/theme/beige.css',
 	'reveal.js/css/theme/black.css',
@@ -22,12 +22,7 @@ Package.onUse(function (api) {
 	'reveal.js/lib/css/zenburn.css',
 	'reveal.js/lib/font/league-gothic/league-gothic.css',
 	'reveal.js/lib/font/source-sans-pro/source-sans-pro.css',
-], 'client', { isAssets: true });
-
-  api.addFiles([
 	'reveal.js/js/reveal.js',
 	'reveal.js/css/reveal.css'
   ], 'client');
-
-  api.export('Reveal', 'client');
 });
